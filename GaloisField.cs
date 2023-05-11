1cccbf8855e955ba599cb97ae6a2e40287f181d5
AMonakovA <artemmonakov@rambler.ru>
2023-05-12 01:28:35 +0300

    commit

---
+++ GaloisField.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BerlekampMassey
+{
+   
+        public static class GaloisField
+        {
+            static readonly int[] logTable = new int[256];
+            static readonly int[] expTable = new int[256];
+
+            static GaloisField()
+            {
+                int x = 1;
+                for (int i = 0; i < 256; i++)
+                {
+                    expTable[i] = x;
+                    logTable[x] = i;
+
+                    x <<= 1;
+                    if (x >= 256)
+                    {
+                        x ^= 0x11B;
+                    }
+                }
+            }
+
+            public static int Multiply(int a, int b)
+            {
+                if (a == 0 || b == 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    int logA = logTable[a];
+                    int logB = logTable[b];
+                    int logResult = (logA + logB) % 255;
+                    return expTable[logResult];
+                }
+            }
+
+            public static int Exp(int power)
+            {
+                power = power % 255;
+                if (power < 0)
+                {
+                    power += 255;
+                }
+                return expTable[power];
+            }
+
+            public static int Log(int value)
+            {
+                if (value == 0)
+                {
+                    throw new ArgumentException("Cannot compute logarithm of zero.");
+                }
+
+                return logTable[value];
+            }
+        }
+    }
+
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿namespace BerlekampMassey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var b = new byte[] { 1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1 };
+            var k = BerlekampMassey(b);
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write(k[i]);
+            }
+            // 1,3,5
+        }
+        public static byte[] BerlekampMassey(byte[] s)
+        {
+            int L, N, m, d;
+            int n = s.Length;
+            byte[] c = new byte[n];
+            byte[] b = new byte[n];
+            byte[] t = new byte[n];
+
+            //Initialization
+            b[0] = c[0] = 1;
+            N = L = 0;
+            m = -1;
+
+            //Algorithm core
+            while (N < n)
+            {
+                d = s[N];
+                for (int i = 1; i <= L; i++)
+                    d ^= c[i] & s[N - i];            //(d+=c[i]*s[N-i] mod 2)
+                if (d == 1)
+                {
+                    Array.Copy(c, t, n);    //T(D)<-C(D)
+                    for (int i = 0; (i + N - m) < n; i++)
+                        c[i + N - m] ^= b[i];
+                    if (L <= (N >> 1))
+                    {
+                        L = N + 1 - L;
+                        m = N;
+                        Array.Copy(t, b, n);    //B(D)<-T(D)
+                    }
+                }
+                N++;
+            }
+            return b;
+        }
+    }
+}
+       
